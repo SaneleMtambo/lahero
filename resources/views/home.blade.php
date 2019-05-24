745875f862060e97a839d9601d6bb649e5454bf4
SaneleMtambo <sanele@dosair.co.za>
2019-05-24 11:17:00 +0200

    examplesite

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,39 +45,31 @@
    	<div class="row header-content">
 
    		<div class="logo">
-	         <a href="index.html">Author</a>
+	         <a href="/home">Author</a>
 	      </div>
 
 	   	<nav id="main-nav-wrap">
 				<ul class="main-navigation sf-menu">
 					<li class="current"><a href="index.html" title="">Home</a></li>									
 					<li class="has-children">
-						<a href="category.html" title="">Categories</a>
+                    <a href="category.html" title="">Categories</a>
 						<ul class="sub-menu">
-			            <li><a href="category.html">Wordpress</a></li>
-			            <li><a href="category.html">HTML</a></li>
-			            <li><a href="category.html">Photography</a></li>
-			            <li><a href="category.html">UI</a></li>
-			            <li><a href="category.html">Mockups</a></li>
-			            <li><a href="category.html">Branding</a></li>
+			            <li><a href="category.html">Black Owned Businesses</a></li>
 			         </ul>
 					</li>
 					<li class="has-children">
-						<a href="single-standard.html" title="">Blog</a>
+						<a href="single-standard.html" title="">Sort By</a>
 						<ul class="sub-menu">
 			            <li><a href="single-video.html">Video Post</a></li>
 			            <li><a href="single-audio.html">Audio Post</a></li>
 			            <li><a href="single-gallery.html">Gallery Post</a></li>
 			            <li><a href="single-standard.html">Standard Post</a></li>
 			         </ul>
-					</li>
-					<li><a href="style-guide.html" title="">Styles</a></li>
-					<li><a href="about.html" title="">About</a></li>	
-                    <li><a href="contact.html" title="">Contact</a></li>
+					</li>	
                     <li class="has-children">
 						<a href="single-standard.html" title="">Profile</a>
 						<ul class="sub-menu">
-			            <li><a href="single-video.html">My Profile</a></li>
+			            <li><a href="/profile">My Profile</a></li>
 			            <li>  <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
